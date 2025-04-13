e98616aab5e509554decfb388600eb98b7e32011
MARIUM BINTE MUHAMMAD <165379776+marium0908@users.noreply.github.com>
2025-04-13 11:12:46 +0600

    Update script.js

--- script.js
+++ script.js
@@ -19,506 +19,88 @@ const quiz = [
     options: ['Bible', 'Quran', 'Torah', 'Vedas'],
     answer: "Quran"
   },
-  {
-    question: "Sample Islamic Question 5?",
-    options: ['Option A5', 'Option B5', 'Option C5', 'Option D5'],
-    answer: "Option A5"
+ {
+    question: "Who was the first Prophet in Islam?",
+    options: ['Prophet Muhammad (PBUH)', 'Prophet Ibrahim (Abraham)', ' Prophet Adam','Prophet Musa (Moses)'],
+    answer: "Prophet Adam"
   },
   {
-    question: "Sample Islamic Question 6?",
-    options: ['Option A6', 'Option B6', 'Option C6', 'Option D6'],
-    answer: "Option A6"
+    question: "Which city is known as the birthplace of Prophet Muhammad (PBUH)?",
+    options: ['Jerusalem', ' Mecca', 'Madina', 'Mairo'],
+    answer: "Mecca"
   },
   {
-    question: "Sample Islamic Question 7?",
-    options: ['Option A7', 'Option B7', 'Option C7', 'Option D7'],
-    answer: "Option A7"
+    question: "In which battle did Prophet Muhammad (PBUH) first participate as a leader?",
+    options: ['Battle of Uhud', 'Battle of Badr', 'Battle of Khandaq', 'Battle of Hunayn'],
+    answer: "Battle of Badr"
   },
   {
-    question: "Sample Islamic Question 8?",
-    options: ['Option A8', 'Option B8', 'Option C8', 'Option D8'],
-    answer: "Option A8"
+    question: "How many pillars of Islam are there?",
+    options: ['5', '3', '2', '7'],
+    answer: "5"
   },
   {
-    question: "Sample Islamic Question 9?",
-    options: ['Option A9', 'Option B9', 'Option C9', 'Option D9'],
-    answer: "Option A9"
+    question: "What is the festival that marks the end of Ramadan?",
+    options: ['Eid al-Adha', 'Eid al-Fitr', 'Mawlid', 'Haji'],
+    answer: "Eid al-Fitr"
   },
   {
-    question: "Sample Islamic Question 10?",
-    options: ['Option A10', 'Option B10', 'Option C10', 'Option D10'],
+    question: "What do Muslims celebrate during Eid al-Adha?",
+    options: [' The end of Hajj', 'The birth of Prophet Muhammad (PBUH)', 'The sacrifice of Prophet Ibrahim', 'The start of the Islamic New Year'],
     answer: "Option A10"
   },
   {
-    question: "Sample Islamic Question 11?",
-    options: ['Option A11', 'Option B11', 'Option C11', 'Option D11'],
-    answer: "Option A11"
+    question: "What percentage of their savings are Muslims required to give to charity?",
+    options: ['1%', '2.5%', '10%', '80%'],
+    answer: "2.5%"
   },
   {
-    question: "Sample Islamic Question 12?",
-    options: ['Option A12', 'Option B12', 'Option C12', 'Option D12'],
-    answer: "Option A12"
+    question: "What is the name of the first revelation Angel?",
+    options: ['Jibrail (A.S.) ', 'Mikail (A.S.)', 'Israfil (A.S.)', 'Azrael (A.S.)'],
+    answer: "Jibrail (A.S.) "
   },
   {
-    question: "Sample Islamic Question 13?",
-    options: ['Option A13', 'Option B13', 'Option C13', 'Option D13'],
+    question: "How many Surahs are there in the Quran?",
+    options: ['110', '120', '114', '300'],
     answer: "Option A13"
   },
   {
-    question: "Sample Islamic Question 14?",
-    options: ['Option A14', 'Option B14', 'Option C14', 'Option D14'],
+    question: "What is the Arabic name of the pilgrimage to Mecca?",
+    options: ['Zakat', 'Haji', 'Fasting', 'Sadaqah'],
     answer: "Option A14"
   },
   {
-    question: "Sample Islamic Question 15?",
-    options: ['Option A15', 'Option B15', 'Option C15', 'Option D15'],
+    question: "What is the name of Prophet Muhammad's (PBUH) mother?",
+    options: ['Halima', 'Amina', 'Fatema', 'Asia'],
     answer: "Option A15"
   },
   {
-    question: "Sample Islamic Question 16?",
-    options: ['Option A16', 'Option B16', 'Option C16', 'Option D16'],
-    answer: "Option A16"
+    question: " What is the Arabic term for charity in Islam?",
+    options: ['Zakat', 'Sawm', 'Haji', 'Salat'],
+    answer: "Zakat"
   },
   {
-    question: "Sample Islamic Question 17?",
-    options: ['Option A17', 'Option B17', 'Option C17', 'Option D17'],
+    question: " What is the name of the Prophet Muhammad’s (PBUH) first wife??",
+    options: ['Aisha(R.A.)', 'Khadija(R.A.)', 'Zainab(R.A.)', 'Fatima(R.A.)'],
     answer: "Option A17"
   },
   {
-    question: "Sample Islamic Question 18?",
-    options: ['Option A18', 'Option B18', 'Option C18', 'Option D18'],
-    answer: "Option A18"
+    question: "What does the word "Islam" mean?",
+    options: ['Peace', 'Submittion', 'Guidance', 'Broterhood'],
+    answer: "Submittion"
   },
   {
-    question: "Sample Islamic Question 19?",
-    options: ['Option A19', 'Option B19', 'Option C19', 'Option D19'],
-    answer: "Option A19"
+    question: "What is the name of the Islamic greeting?",
+    options: ['Solom', 'Hola', 'Salam', 'Adab'],
+    answer: "Salam"
   },
   {
-    question: "Sample Islamic Question 20?",
-    options: ['Option A20', 'Option B20', 'Option C20', 'Option D20'],
-    answer: "Option A20"
+    question: "What does the word "Zakat" mean in English?",
+    options: ['Charity', 'Fasting', 'Prayer', 'Suplication'],
+    answer: "Charity"
   },
-  {
-    question: "Sample Islamic Question 21?",
-    options: ['Option A21', 'Option B21', 'Option C21', 'Option D21'],
-    answer: "Option A21"
-  },
-  {
-    question: "Sample Islamic Question 22?",
-    options: ['Option A22', 'Option B22', 'Option C22', 'Option D22'],
-    answer: "Option A22"
-  },
-  {
-    question: "Sample Islamic Question 23?",
-    options: ['Option A23', 'Option B23', 'Option C23', 'Option D23'],
-    answer: "Option A23"
-  },
-  {
-    question: "Sample Islamic Question 24?",
-    options: ['Option A24', 'Option B24', 'Option C24', 'Option D24'],
-    answer: "Option A24"
-  },
-  {
-    question: "Sample Islamic Question 25?",
-    options: ['Option A25', 'Option B25', 'Option C25', 'Option D25'],
-    answer: "Option A25"
-  },
-  {
-    question: "Sample Islamic Question 26?",
-    options: ['Option A26', 'Option B26', 'Option C26', 'Option D26'],
-    answer: "Option A26"
-  },
-  {
-    question: "Sample Islamic Question 27?",
-    options: ['Option A27', 'Option B27', 'Option C27', 'Option D27'],
-    answer: "Option A27"
-  },
-  {
-    question: "Sample Islamic Question 28?",
-    options: ['Option A28', 'Option B28', 'Option C28', 'Option D28'],
-    answer: "Option A28"
-  },
-  {
-    question: "Sample Islamic Question 29?",
-    options: ['Option A29', 'Option B29', 'Option C29', 'Option D29'],
-    answer: "Option A29"
-  },
-  {
-    question: "Sample Islamic Question 30?",
-    options: ['Option A30', 'Option B30', 'Option C30', 'Option D30'],
-    answer: "Option A30"
-  },
-  {
-    question: "Sample Islamic Question 31?",
-    options: ['Option A31', 'Option B31', 'Option C31', 'Option D31'],
-    answer: "Option A31"
-  },
-  {
-    question: "Sample Islamic Question 32?",
-    options: ['Option A32', 'Option B32', 'Option C32', 'Option D32'],
-    answer: "Option A32"
-  },
-  {
-    question: "Sample Islamic Question 33?",
-    options: ['Option A33', 'Option B33', 'Option C33', 'Option D33'],
-    answer: "Option A33"
-  },
-  {
-    question: "Sample Islamic Question 34?",
-    options: ['Option A34', 'Option B34', 'Option C34', 'Option D34'],
-    answer: "Option A34"
-  },
-  {
-    question: "Sample Islamic Question 35?",
-    options: ['Option A35', 'Option B35', 'Option C35', 'Option D35'],
-    answer: "Option A35"
-  },
-  {
-    question: "Sample Islamic Question 36?",
-    options: ['Option A36', 'Option B36', 'Option C36', 'Option D36'],
-    answer: "Option A36"
-  },
-  {
-    question: "Sample Islamic Question 37?",
-    options: ['Option A37', 'Option B37', 'Option C37', 'Option D37'],
-    answer: "Option A37"
-  },
-  {
-    question: "Sample Islamic Question 38?",
-    options: ['Option A38', 'Option B38', 'Option C38', 'Option D38'],
-    answer: "Option A38"
-  },
-  {
-    question: "Sample Islamic Question 39?",
-    options: ['Option A39', 'Option B39', 'Option C39', 'Option D39'],
-    answer: "Option A39"
-  },
-  {
-    question: "Sample Islamic Question 40?",
-    options: ['Option A40', 'Option B40', 'Option C40', 'Option D40'],
-    answer: "Option A40"
-  },
-  {
-    question: "Sample Islamic Question 41?",
-    options: ['Option A41', 'Option B41', 'Option C41', 'Option D41'],
-    answer: "Option A41"
-  },
-  {
-    question: "Sample Islamic Question 42?",
-    options: ['Option A42', 'Option B42', 'Option C42', 'Option D42'],
-    answer: "Option A42"
-  },
-  {
-    question: "Sample Islamic Question 43?",
-    options: ['Option A43', 'Option B43', 'Option C43', 'Option D43'],
-    answer: "Option A43"
-  },
-  {
-    question: "Sample Islamic Question 44?",
-    options: ['Option A44', 'Option B44', 'Option C44', 'Option D44'],
-    answer: "Option A44"
-  },
-  {
-    question: "Sample Islamic Question 45?",
-    options: ['Option A45', 'Option B45', 'Option C45', 'Option D45'],
-    answer: "Option A45"
-  },
-  {
-    question: "Sample Islamic Question 46?",
-    options: ['Option A46', 'Option B46', 'Option C46', 'Option D46'],
-    answer: "Option A46"
-  },
-  {
-    question: "Sample Islamic Question 47?",
-    options: ['Option A47', 'Option B47', 'Option C47', 'Option D47'],
-    answer: "Option A47"
-  },
-  {
-    question: "Sample Islamic Question 48?",
-    options: ['Option A48', 'Option B48', 'Option C48', 'Option D48'],
-    answer: "Option A48"
-  },
-  {
-    question: "Sample Islamic Question 49?",
-    options: ['Option A49', 'Option B49', 'Option C49', 'Option D49'],
-    answer: "Option A49"
-  },
-  {
-    question: "Sample Islamic Question 50?",
-    options: ['Option A50', 'Option B50', 'Option C50', 'Option D50'],
-    answer: "Option A50"
-  },
-  {
-    question: "Sample Islamic Question 51?",
-    options: ['Option A51', 'Option B51', 'Option C51', 'Option D51'],
-    answer: "Option A51"
-  },
-  {
-    question: "Sample Islamic Question 52?",
-    options: ['Option A52', 'Option B52', 'Option C52', 'Option D52'],
-    answer: "Option A52"
-  },
-  {
-    question: "Sample Islamic Question 53?",
-    options: ['Option A53', 'Option B53', 'Option C53', 'Option D53'],
-    answer: "Option A53"
-  },
-  {
-    question: "Sample Islamic Question 54?",
-    options: ['Option A54', 'Option B54', 'Option C54', 'Option D54'],
-    answer: "Option A54"
-  },
-  {
-    question: "Sample Islamic Question 55?",
-    options: ['Option A55', 'Option B55', 'Option C55', 'Option D55'],
-    answer: "Option A55"
-  },
-  {
-    question: "Sample Islamic Question 56?",
-    options: ['Option A56', 'Option B56', 'Option C56', 'Option D56'],
-    answer: "Option A56"
-  },
-  {
-    question: "Sample Islamic Question 57?",
-    options: ['Option A57', 'Option B57', 'Option C57', 'Option D57'],
-    answer: "Option A57"
-  },
-  {
-    question: "Sample Islamic Question 58?",
-    options: ['Option A58', 'Option B58', 'Option C58', 'Option D58'],
-    answer: "Option A58"
-  },
-  {
-    question: "Sample Islamic Question 59?",
-    options: ['Option A59', 'Option B59', 'Option C59', 'Option D59'],
-    answer: "Option A59"
-  },
-  {
-    question: "Sample Islamic Question 60?",
-    options: ['Option A60', 'Option B60', 'Option C60', 'Option D60'],
-    answer: "Option A60"
-  },
-  {
-    question: "Sample Islamic Question 61?",
-    options: ['Option A61', 'Option B61', 'Option C61', 'Option D61'],
-    answer: "Option A61"
-  },
-  {
-    question: "Sample Islamic Question 62?",
-    options: ['Option A62', 'Option B62', 'Option C62', 'Option D62'],
-    answer: "Option A62"
-  },
-  {
-    question: "Sample Islamic Question 63?",
-    options: ['Option A63', 'Option B63', 'Option C63', 'Option D63'],
-    answer: "Option A63"
-  },
-  {
-    question: "Sample Islamic Question 64?",
-    options: ['Option A64', 'Option B64', 'Option C64', 'Option D64'],
-    answer: "Option A64"
-  },
-  {
-    question: "Sample Islamic Question 65?",
-    options: ['Option A65', 'Option B65', 'Option C65', 'Option D65'],
-    answer: "Option A65"
-  },
-  {
-    question: "Sample Islamic Question 66?",
-    options: ['Option A66', 'Option B66', 'Option C66', 'Option D66'],
-    answer: "Option A66"
-  },
-  {
-    question: "Sample Islamic Question 67?",
-    options: ['Option A67', 'Option B67', 'Option C67', 'Option D67'],
-    answer: "Option A67"
-  },
-  {
-    question: "Sample Islamic Question 68?",
-    options: ['Option A68', 'Option B68', 'Option C68', 'Option D68'],
-    answer: "Option A68"
-  },
-  {
-    question: "Sample Islamic Question 69?",
-    options: ['Option A69', 'Option B69', 'Option C69', 'Option D69'],
-    answer: "Option A69"
-  },
-  {
-    question: "Sample Islamic Question 70?",
-    options: ['Option A70', 'Option B70', 'Option C70', 'Option D70'],
-    answer: "Option A70"
-  },
-  {
-    question: "Sample Islamic Question 71?",
-    options: ['Option A71', 'Option B71', 'Option C71', 'Option D71'],
-    answer: "Option A71"
-  },
-  {
-    question: "Sample Islamic Question 72?",
-    options: ['Option A72', 'Option B72', 'Option C72', 'Option D72'],
-    answer: "Option A72"
-  },
-  {
-    question: "Sample Islamic Question 73?",
-    options: ['Option A73', 'Option B73', 'Option C73', 'Option D73'],
-    answer: "Option A73"
-  },
-  {
-    question: "Sample Islamic Question 74?",
-    options: ['Option A74', 'Option B74', 'Option C74', 'Option D74'],
-    answer: "Option A74"
-  },
-  {
-    question: "Sample Islamic Question 75?",
-    options: ['Option A75', 'Option B75', 'Option C75', 'Option D75'],
-    answer: "Option A75"
-  },
-  {
-    question: "Sample Islamic Question 76?",
-    options: ['Option A76', 'Option B76', 'Option C76', 'Option D76'],
-    answer: "Option A76"
-  },
-  {
-    question: "Sample Islamic Question 77?",
-    options: ['Option A77', 'Option B77', 'Option C77', 'Option D77'],
-    answer: "Option A77"
-  },
-  {
-    question: "Sample Islamic Question 78?",
-    options: ['Option A78', 'Option B78', 'Option C78', 'Option D78'],
-    answer: "Option A78"
-  },
-  {
-    question: "Sample Islamic Question 79?",
-    options: ['Option A79', 'Option B79', 'Option C79', 'Option D79'],
-    answer: "Option A79"
-  },
-  {
-    question: "Sample Islamic Question 80?",
-    options: ['Option A80', 'Option B80', 'Option C80', 'Option D80'],
-    answer: "Option A80"
-  },
-  {
-    question: "Sample Islamic Question 81?",
-    options: ['Option A81', 'Option B81', 'Option C81', 'Option D81'],
-    answer: "Option A81"
-  },
-  {
-    question: "Sample Islamic Question 82?",
-    options: ['Option A82', 'Option B82', 'Option C82', 'Option D82'],
-    answer: "Option A82"
-  },
-  {
-    question: "Sample Islamic Question 83?",
-    options: ['Option A83', 'Option B83', 'Option C83', 'Option D83'],
-    answer: "Option A83"
-  },
-  {
-    question: "Sample Islamic Question 84?",
-    options: ['Option A84', 'Option B84', 'Option C84', 'Option D84'],
-    answer: "Option A84"
-  },
-  {
-    question: "Sample Islamic Question 85?",
-    options: ['Option A85', 'Option B85', 'Option C85', 'Option D85'],
-    answer: "Option A85"
-  },
-  {
-    question: "Sample Islamic Question 86?",
-    options: ['Option A86', 'Option B86', 'Option C86', 'Option D86'],
-    answer: "Option A86"
-  },
-  {
-    question: "Sample Islamic Question 87?",
-    options: ['Option A87', 'Option B87', 'Option C87', 'Option D87'],
-    answer: "Option A87"
-  },
-  {
-    question: "Sample Islamic Question 88?",
-    options: ['Option A88', 'Option B88', 'Option C88', 'Option D88'],
-    answer: "Option A88"
-  },
-  {
-    question: "Sample Islamic Question 89?",
-    options: ['Option A89', 'Option B89', 'Option C89', 'Option D89'],
-    answer: "Option A89"
-  },
-  {
-    question: "Sample Islamic Question 90?",
-    options: ['Option A90', 'Option B90', 'Option C90', 'Option D90'],
-    answer: "Option A90"
-  },
-  {
-    question: "Sample Islamic Question 91?",
-    options: ['Option A91', 'Option B91', 'Option C91', 'Option D91'],
-    answer: "Option A91"
-  },
-  {
-    question: "Sample Islamic Question 92?",
-    options: ['Option A92', 'Option B92', 'Option C92', 'Option D92'],
-    answer: "Option A92"
-  },
-  {
-    question: "Sample Islamic Question 93?",
-    options: ['Option A93', 'Option B93', 'Option C93', 'Option D93'],
-    answer: "Option A93"
-  },
-  {
-    question: "Sample Islamic Question 94?",
-    options: ['Option A94', 'Option B94', 'Option C94', 'Option D94'],
-    answer: "Option A94"
-  },
-  {
-    question: "Sample Islamic Question 95?",
-    options: ['Option A95', 'Option B95', 'Option C95', 'Option D95'],
-    answer: "Option A95"
-  },
-  {
-    question: "Sample Islamic Question 96?",
-    options: ['Option A96', 'Option B96', 'Option C96', 'Option D96'],
-    answer: "Option A96"
-  },
-  {
-    question: "Sample Islamic Question 97?",
-    options: ['Option A97', 'Option B97', 'Option C97', 'Option D97'],
-    answer: "Option A97"
-  },
-  {
-    question: "Sample Islamic Question 98?",
-    options: ['Option A98', 'Option B98', 'Option C98', 'Option D98'],
-    answer: "Option A98"
-  },
-  {
-    question: "Sample Islamic Question 99?",
-    options: ['Option A99', 'Option B99', 'Option C99', 'Option D99'],
-    answer: "Option A99"
-  },
-  {
-    question: "Sample Islamic Question 100?",
-    options: ['Option A100', 'Option B100', 'Option C100', 'Option D100'],
-    answer: "Option A100"
-  },
-  {
-    question: "Sample Islamic Question 101?",
-    options: ['Option A101', 'Option B101', 'Option C101', 'Option D101'],
-    answer: "Option A101"
-  },
-  {
-    question: "Sample Islamic Question 102?",
-    options: ['Option A102', 'Option B102', 'Option C102', 'Option D102'],
-    answer: "Option A102"
-  },
-  {
-    question: "Sample Islamic Question 103?",
-    options: ['Option A103', 'Option B103', 'Option C103', 'Option D103'],
-    answer: "Option A103"
-  },
-  {
-    question: "Sample Islamic Question 104?",
-    options: ['Option A104', 'Option B104', 'Option C104', 'Option D104'],
-    answer: "Option A104"
-  },
+  
+  
 ];
 
 let currentQuestion = 0;
